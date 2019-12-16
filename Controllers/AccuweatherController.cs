ef68aee2f7bb146cfa76f63e1d3ca690d04ed62c
Asaf Shmaya <sh_Asaf@hotmail.com>
2019-12-16 23:25:46 +0200

    Added css borders Added search functions to site js Saved results

--- Controllers/AccuweatherController.cs
+++ Controllers/AccuweatherController.cs
@@ -94,99 +94,99 @@ namespace Accuweather.Controllers
 
 
 
-        // GET: api/Accuweather
-        public IQueryable<tbl_Favories> Gettbl_Favories()
-        {
-            return db.tbl_Favories;
-        }
+        //// GET: api/Accuweather
+        //public IQueryable<tbl_Favories> Gettbl_Favories()
+        //{
+        //    return db.tbl_Favories;
+        //}
 
-        // GET: api/Accuweather/5
-        [ResponseType(typeof(tbl_Favories))]
-        public async Task<IHttpActionResult> Gettbl_Favories(int id)
-        {
-            tbl_Favories tbl_Favories = await db.tbl_Favories.FindAsync(id);
-            if (tbl_Favories == null)
-            {
-                return NotFound();
-            }
+        //// GET: api/Accuweather/5
+        //[ResponseType(typeof(tbl_Favories))]
+        //public async Task<IHttpActionResult> Gettbl_Favories(int id)
+        //{
+        //    tbl_Favories tbl_Favories = await db.tbl_Favories.FindAsync(id);
+        //    if (tbl_Favories == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return Ok(tbl_Favories);
-        }
+        //    return Ok(tbl_Favories);
+        //}
 
-        // PUT: api/Accuweather/5
-        [ResponseType(typeof(void))]
-        public async Task<IHttpActionResult> Puttbl_Favories(tbl_Favories tbl_Favories)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+        //// PUT: api/Accuweather/5
+        //[ResponseType(typeof(void))]
+        //public async Task<IHttpActionResult> Puttbl_Favories(tbl_Favories tbl_Favories)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
 
-            db.Entry(tbl_Favories).State = EntityState.Modified;
+        //    db.Entry(tbl_Favories).State = EntityState.Modified;
 
-            try
-            {
-                await db.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!tbl_FavoriesExists(tbl_Favories.LocationKey))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+        //    try
+        //    {
+        //        await db.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!tbl_FavoriesExists(tbl_Favories.LocationKey))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
 
-            return StatusCode(HttpStatusCode.NoContent);
-        }
+        //    return StatusCode(HttpStatusCode.NoContent);
+        //}
 
-        // POST: api/Accuweather
-        [ResponseType(typeof(tbl_Favories))]
-        public async Task<IHttpActionResult> Posttbl_Favories(tbl_Favories tbl_Favories)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+        //// POST: api/Accuweather
+        //[ResponseType(typeof(tbl_Favories))]
+        //public async Task<IHttpActionResult> Posttbl_Favories(tbl_Favories tbl_Favories)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
 
-            db.tbl_Favories.Add(tbl_Favories);
-            await db.SaveChangesAsync();
+        //    db.tbl_Favories.Add(tbl_Favories);
+        //    await db.SaveChangesAsync();
 
-            return CreatedAtRoute("DefaultApi", new { id = tbl_Favories.LocationKey }, tbl_Favories);
-        }
+        //    return CreatedAtRoute("DefaultApi", new { id = tbl_Favories.LocationKey }, tbl_Favories);
+        //}
 
-        // DELETE: api/Accuweather/5
-        [ResponseType(typeof(tbl_Favories))]
-        public async Task<IHttpActionResult> Deletetbl_Favories(tbl_Favories tbl_FavoriesFromClient)
-        {
-            tbl_Favories tbl_Favories = await db.tbl_Favories.FindAsync(tbl_FavoriesFromClient.LocationKey);
+        //// DELETE: api/Accuweather/5
+        //[ResponseType(typeof(tbl_Favories))]
+        //public async Task<IHttpActionResult> Deletetbl_Favories(tbl_Favories tbl_FavoriesFromClient)
+        //{
+        //    tbl_Favories tbl_Favories = await db.tbl_Favories.FindAsync(tbl_FavoriesFromClient.LocationKey);
 
-            if (tbl_Favories == null)
-            {
-                return NotFound();
-            }
+        //    if (tbl_Favories == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            db.tbl_Favories.Remove(tbl_Favories);
-            await db.SaveChangesAsync();
+        //    db.tbl_Favories.Remove(tbl_Favories);
+        //    await db.SaveChangesAsync();
 
-            return Ok(tbl_Favories);
-        }
+        //    return Ok(tbl_Favories);
+        //}
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        db.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
 
-        private bool tbl_FavoriesExists(int id)
-        {
-            return db.tbl_Favories.Count(e => e.LocationKey == id) > 0;
-        }
+        //private bool tbl_FavoriesExists(int id)
+        //{
+        //    return db.tbl_Favories.Count(e => e.LocationKey == id) > 0;
+        //}
     }
 }
--- Scripts/site.js
+++ Scripts/site.js
@@ -1,1 +1,83 @@
-﻿
+﻿var api = [];
+api.search = "https://localhost:44351/api/accuweather/search?";
+api.searchExampleResponseToLocationTel = {
+    data: [
+        {
+            locationKey: 215854,
+            localizedName: 'Tel Aviv',
+        },
+        {
+            locationKey: 3431644,
+            localizedName: 'Telanaipura',
+        },
+        {
+            locationKey: 300558,
+            localizedName: 'Telok Blangah New Town',
+        },
+        {
+            locationKey: 325876,
+            localizedName: 'Telford',
+        },
+        {
+            locationKey: 169072,
+            localizedName: 'Telavi',
+        },
+        {
+            locationKey: 230611,
+            localizedName: 'Telsiai',
+        },
+        {
+            locationKey: 2723742,
+            localizedName: 'Telégrafo',
+        },
+        {
+            locationKey: 186933,
+            localizedName: 'Tela',
+        },
+        {
+            locationKey: 3453754,
+            localizedName: 'Telaga Asih',
+        },
+        {
+            locationKey: 3453755,
+            localizedName: 'Telagamurni',
+        },
+    ],
+};
+
+var search = [];
+search.attachEvent = function () {
+    var txtLocation = api.search + $('#txtLocation').val();
+    //
+    $('#btn-search').click(function () {
+        $.ajax({
+            method: 'GET',
+            url: api.search,
+            data: { location: $('#txtLocation').val() },
+            success: function (response) {
+                search.populateResults(response);
+            },
+            error: function (xhr, status) {
+                console.log('statusText: ' + xhr.statusText);
+            }
+        });
+    });
+};
+
+search.populateResults = function (results) {
+    if (!results) { results = api.searchExampleResponseToLocationTel; }
+
+    results.data.forEach(function (item, index) {
+        // GET TEMPLATE CLONE
+        var searchResultTemplate = $($('#templates #locations-template')[0].outerHTML).clone();
+        // REPLACE PLACEHOLDERS TEXT / VALUES
+        searchResultTemplate.attr('locationkey', item.locationKey)
+        searchResultTemplate.find('p').text(item.localizedName)
+        // ATTACH TO DESIGNATED PLACE
+        searchResultTemplate.appendTo($("#locationsSearchResults"));
+    }); 
+};
+
+$(function () {
+    search.attachEvent();
+});
