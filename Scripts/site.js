fb2a2193fbc8af1662bd36cd4f35e0019e320bd9
Asaf Shmaya <sh_Asaf@hotmail.com>
2019-12-17 00:29:33 +0200

    Added current conditions

--- Scripts/site.js
+++ Scripts/site.js
@@ -1,5 +1,5 @@
 ﻿var api = [];
-api.search = "https://localhost:44351/api/accuweather/search?";
+api.searchUrl = 'https://localhost:44351/api/accuweather/search?';
 api.searchExampleResponseToLocationTel = {
     data: [
         {
@@ -44,15 +44,27 @@ api.searchExampleResponseToLocationTel = {
         },
     ],
 };
+api.currentConditionsUrl = 'https://localhost:44351/api/accuweather/getCurrentWeather?';
+api.currentConditionsExampleResponse = {
+    data: [
+        {
+            celsiusTemperature: 12.9,
+            weatherText: 'Mostly clear',
+        },
+    ],
+};
 
+/*
+ *  api.currentConditionsExampleResponse.data[0].celsiusTemperature
+    api.currentConditionsExampleResponse.data[0].weatherText
+ */
+ 
 var search = [];
 search.attachEvent = function () {
-    var txtLocation = api.search + $('#txtLocation').val();
-    //
     $('#btn-search').click(function () {
         $.ajax({
             method: 'GET',
-            url: api.search,
+            url: api.searchUrl,
             data: { location: $('#txtLocation').val() },
             success: function (response) {
                 search.populateResults(response);
@@ -70,12 +82,46 @@ search.populateResults = function (results) {
     results.data.forEach(function (item, index) {
         // GET TEMPLATE CLONE
         var searchResultTemplate = $($('#templates #locations-template')[0].outerHTML).clone();
+        // REMOVE ID FROM TEMPLATE GENERATION
+        searchResultTemplate.removeAttr('id');
         // REPLACE PLACEHOLDERS TEXT / VALUES
-        searchResultTemplate.attr('locationkey', item.locationKey)
-        searchResultTemplate.find('p').text(item.localizedName)
+        searchResultTemplate.attr('locationkey', item.locationKey);
+        searchResultTemplate.find('a').text(item.localizedName);
         // ATTACH TO DESIGNATED PLACE
         searchResultTemplate.appendTo($("#locationsSearchResults"));
     }); 
+
+    currentConditions.attachEvent();
+};
+
+var currentConditions = [];
+currentConditions.localizedName = 'Tesla';
+currentConditions.attachEvent = function () {
+    $('#locationsSearchResults .locations-template.panel').click(function () {
+        currentConditions.localizedName = $(this)[0].outerText;
+        $.ajax({
+            method: 'GET',
+            url: api.currentConditionsUrl,
+            data: { locationKey: $($(this)[0]).attr('locationkey') },
+            success: function (response) {
+                currentConditions.populateResults(response);
+            },
+            error: function (xhr, status) {
+                console.log('statusText: ' + xhr.statusText);
+            }
+        });
+    });
+};
+currentConditions.populateResults = function (results) {
+    if (!results) { results = api.currentConditionsExampleResponse; currentConditions.localizedName = 'Stored City of Tel Aviv' }
+
+    // ASSIGN VALUES
+    $($('#current-conditions-template p#localizedName')[0]).text(currentConditions.localizedName);
+    $($('#current-conditions-template span#celsiusTemperature')[0]).text(results.data[0].celsiusTemperature);
+    $($('#current-conditions-template span#weatherText')[0]).text(results.data[0].weatherText);
+   
+    // UNHIDE CURRENT CONDITIONS ELEMENT
+    $('#current-conditions-template').removeClass('hidden');
 };
 
 $(function () {
